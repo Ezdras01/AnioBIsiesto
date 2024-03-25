515fcda99d172de0cb3699fbb9bb17415a9e0035
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2024-03-25 08:53:19 -0600

    Condiciones de Uso
    Se agregaron condiciones de uso para y se definió la función del programa

---
+++ Main_Bisiesto.py
@@ -0,0 +1,8 @@
+def aniobisiesto():
+    anio = int(input("Introduce un año: "))
+    if anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0):
+        print("El año es bisiesto")
+    else:
+        print("El año no es bisiesto")
+        
+aniobisiesto()
