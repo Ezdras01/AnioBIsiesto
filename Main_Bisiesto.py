80f0750887085e162a70efe94309e95b0f944484
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2024-03-25 09:22:06 -0600

    se agrgo una Función Bisiesto
    Se agrego la función bisiesto

--- Main_Bisiesto.py
+++ Main_Bisiesto.py
@@ -1,18 +1,20 @@
-print("Programa que determina si un año es bisiesto o no")
+def bisiesto(anio):
+    if anio < 1582:
+        print("Por favor, introduce un año válido")
+    elif anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0):
+        
+        return print("El año es bisiesto")
+    else:
+        print("El año no es bisiesto")
+    return 0
 
+print("Programa que determina si un año es bisiesto o no")
 año = None
 while True:
     if año == None:
         try:
             anio = int(input("Introduce un año: "))
+            bisiesto(anio)          
         except ValueError:
             print("Por favor, introduce un año válido")
             continue
-    if anio < 1582:
-        print("Por favor, introduce un año válido")
-        continue 
-    elif anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0):
-        print("El año es bisiesto")
-    else:
-        print("El año no es bisiesto")
-        
