5283aa25ec7124e1cad1e9fef20d54ee8ee50008
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2024-03-25 09:10:35 -0600

    remover función
    Se removió la función y se le agrego unas restricciones  de funciones

--- Main_Bisiesto.py
+++ Main_Bisiesto.py
@@ -1,8 +1,18 @@
-def aniobisiesto():
-    anio = int(input("Introduce un año: "))
-    if anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0):
+print("Programa que determina si un año es bisiesto o no")
+
+año = None
+while True:
+    if año == None:
+        try:
+            anio = int(input("Introduce un año: "))
+        except ValueError:
+            print("Por favor, introduce un año válido")
+            continue
+    if anio < 1582:
+        print("Por favor, introduce un año válido")
+        continue 
+    elif anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0):
         print("El año es bisiesto")
     else:
         print("El año no es bisiesto")
         
-aniobisiesto()
